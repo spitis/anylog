6f97100eef13846fc306aab8c46645392b57b33f
spitis <silviu.pitis@gmail.com>
2016-06-04 22:17:53 -0400

    small production changes

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo add-apt-repository ppa:fkrull/deadsnakes
+sudo apt-get update
+sudo apt-get install python3.5
+sudo apt-get install python3.5-dev
+sudo apt-get install python-psycopg2
+sudo apt-get install postgresql-9.5 postgresql-client-9.5 postgresql-contrib-9.5 postgresql-server-dev-9.5
+#Then also need to set up postgres database
