35eb9ea45b00fcacd4f5662d70baa18e4e1c10a9
spitis <silviu.pitis@gmail.com>
2016-06-07 16:35:58 -0400

    fix forms

--- client/components/AddLogForm.jsx
+++ client/components/AddLogForm.jsx
@@ -8,48 +8,51 @@ import {
   Button,
 } from 'react-bootstrap';
 
-const addLogForm = (props) => {
-  const {
-      fields: {
-        eventName,
-        eventText,
-      },
-      addLogHandler,
-    } = props;
+class addLogForm extends React.Component {
 
-  return (
-    <div>
-      <form onSubmit={addLogHandler} ref="form">
-        <FormGroup controlId="addLog-eventName">
-          <ControlLabel>Event Name</ControlLabel>
-          <FormControl
-            {...eventName}
-            type="text"
-            placeholder=""
-            required
-          />
-        </FormGroup>
-        <FormGroup controlId="addLog-eventText">
-          <ControlLabel>Event Text</ControlLabel>
-          <FormControl
-            {...eventText}
-            type="text"
-            placeholder="Optional"
-          />
-        </FormGroup>
-        <Button
-          onTouchEnd={this.refs.form.getDOMNode()
-                      .dispatchEvent(new Event('submit'))}
-          type="submit"
-          bsStyle="primary"
-          block
-        >
-          Add log
-        </Button>
-      </form>
-    </div>
-  );
-};
+  render() {
+    const {
+        fields: {
+          eventName,
+          eventText,
+        },
+        addLogHandler,
+      } = this.props;
+
+    return (
+      <div>
+        <form onSubmit={addLogHandler} ref="form">
+          <FormGroup controlId="addLog-eventName">
+            <ControlLabel>Event Name</ControlLabel>
+            <FormControl
+              {...eventName}
+              type="text"
+              placeholder=""
+              required
+            />
+          </FormGroup>
+          <FormGroup controlId="addLog-eventText">
+            <ControlLabel>Event Text</ControlLabel>
+            <FormControl
+              {...eventText}
+              type="text"
+              placeholder="Optional"
+            />
+          </FormGroup>
+          <Button
+            onTouchEnd={this.refs.form.getDOMNode()
+                        .dispatchEvent(new Event('submit'))}
+            type="submit"
+            bsStyle="primary"
+            block
+          >
+            Add log
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
 
 const AddLogForm = reduxForm({
   form: 'addLog',
--- client/components/CreateAccountForm.jsx
+++ client/components/CreateAccountForm.jsx
@@ -8,52 +8,58 @@ import {
   Button,
 } from 'react-bootstrap';
 
-const createAccountForm = (props) => {
-  const { fields: { username, email, password }, createAccountHandler } = props;
+class createAccountForm extends React.Component {
 
-  return (
-    <div>
-      <form onSubmit={createAccountHandler} ref="form">
-        <FormGroup controlId="login-username">
-          <ControlLabel srOnly>Username</ControlLabel>
-          <FormControl
-            {...username}
-            type="text"
-            placeholder="Username"
-            required
-          />
-        </FormGroup>
-        <FormGroup controlId="login-email">
-          <ControlLabel srOnly>Email address</ControlLabel>
-          <FormControl
-            {...email}
-            type="email"
-            placeholder="Email"
-            required
-          />
-        </FormGroup>
-        <FormGroup controlId="login-password">
-          <ControlLabel srOnly>Password</ControlLabel>
-          <FormControl
-            {...password}
-            type="password"
-            placeholder="Password"
-            required
-          />
-        </FormGroup>
-        <Button
-          onTouchEnd={this.refs.form.getDOMNode()
-                      .dispatchEvent(new Event('submit'))}
-          type="submit"
-          bsStyle="primary"
-          block
-        >
-          Create account
-        </Button>
-      </form>
-    </div>
-  );
-};
+  render() {
+    const {
+      fields: { username, email, password },
+      createAccountHandler,
+    } = this.props;
+
+    return (
+      <div>
+        <form onSubmit={createAccountHandler} ref="form">
+          <FormGroup controlId="login-username">
+            <ControlLabel srOnly>Username</ControlLabel>
+            <FormControl
+              {...username}
+              type="text"
+              placeholder="Username"
+              required
+            />
+          </FormGroup>
+          <FormGroup controlId="login-email">
+            <ControlLabel srOnly>Email address</ControlLabel>
+            <FormControl
+              {...email}
+              type="email"
+              placeholder="Email"
+              required
+            />
+          </FormGroup>
+          <FormGroup controlId="login-password">
+            <ControlLabel srOnly>Password</ControlLabel>
+            <FormControl
+              {...password}
+              type="password"
+              placeholder="Password"
+              required
+            />
+          </FormGroup>
+          <Button
+            onTouchEnd={this.refs.form.getDOMNode()
+                        .dispatchEvent(new Event('submit'))}
+            type="submit"
+            bsStyle="primary"
+            block
+          >
+            Create account
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
 
 const CreateAccountForm = reduxForm({
   form: 'createAccount',
