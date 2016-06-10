eb76bee72e7858034de6b2533ce65a90c9494937
spitis <silviu.pitis@gmail.com>
2016-06-10 15:55:14 -0400

    config changes

--- anylog/__init__.py
+++ anylog/__init__.py
@@ -8,8 +8,10 @@
 
 """
 
+import requests
 from flask import Flask, render_template
 from flask_cors import CORS
+from flask_mail import Mail
 import sqlalchemy as sa
 from sqlalchemy.engine.url import URL
 
@@ -23,21 +25,8 @@ from .api.models import db
 """
 
 app = Flask(__name__)
-CORS(app)
 
-app.config.update(dict(
-    DEBUG=True,
-    SECRET_KEY='development key',
-
-    DB_DRIVER='postgresql',
-    DB_HOST='localhost',
-    DB_PORT='5432',
-    DB_USER='postgres',
-    DB_PASS='alpha',
-    DB_NAME='anylog',
-
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-))
+app.config.from_envvar('ANYLOG_SETTINGS')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     str(URL(
@@ -49,48 +38,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
         database=app.config['DB_NAME']
     ))
 
-app.config.from_envvar('ANYLOG_SETTINGS', silent=True)
-
 db.init_app(app)
 
-"""
-
-    APP SETUP AND STARTUP
-
-"""
-
-def create_engine():
-    """
-    Returns an engine connected to DB
-    """
-    return sa.create_engine(
-        URL(
-            drivername=app.config['DB_DRIVER'],
-            host=app.config['DB_HOST'],
-            port=app.config['DB_PORT'],
-            username=app.config['DB_USER'],
-            password=app.config['DB_PASS'],
-            database=app.config['DB_NAME']
-        )
-    )
-
-def connect_db():
-    """
-    Connects app to DB
-    """
-    app.engine = create_engine()
-    app.metadata = sa.MetaData(bind=app.engine)
-    app.metadata.reflect()
-
-def reset_db(testing=True):
-    """
-    Drops and recreates all tables in DB
-    """
-    if testing:
-        app.config['DB_NAME'] = 'anylog_test'
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
+CORS(app)
+mail = Mail(app)
 
 """
 
@@ -105,6 +56,18 @@ app.register_blueprint(api, url_prefix='/api/v0.2')
     APP LAUNCH
 
 """
+
+from flask_mail import Message
+@app.route('/mailtosilviu')
+def mailtosilviu(mailto= ['silviu.pitis@gmail.com'], mailfrom=app.config['MAIL_DEFAULT_SENDER']):
+    requests.post(
+        app.config['MAILGUN_BASE_URL']+"/messages",
+        auth=("api", app.config['MAILGUN_API_KEY']),
+        data={"from": mailfrom,
+              "to": mailto,
+              "subject": "Hello",
+              "text": "Testing some Mailgun awesomness!"})
+    return 'OK', 200
 
 @app.route('/')
 @app.route('/<path:path>')
